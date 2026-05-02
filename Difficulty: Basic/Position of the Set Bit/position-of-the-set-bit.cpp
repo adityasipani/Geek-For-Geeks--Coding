468d6f6f78dce1ea76fe3e88b188ef5386d11710
Aditya Sipani <asipani969@gmail.com>
2026-05-02 10:23:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Position of the Set Bit/position-of-the-set-bit.cpp
+++ Difficulty: Basic/Position of the Set Bit/position-of-the-set-bit.cpp
@@ -1,15 +1,9 @@
 class Solution {
   public:
     int findPosition(int n) {
-        if (n == 0 || (n & (n - 1)) != 0)
+        if (n == 0 || (n & (n - 1)))
             return -1;
-
-        int pos = 1;
-        while ((n & 1) == 0) {
-            n >>= 1;
-            pos++;
-        }
-        
-        return pos;
+            
+        return __builtin_ctz(n) + 1;
     }
 };
